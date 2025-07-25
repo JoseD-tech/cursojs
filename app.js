0a17e544250a3b059a1c087e8d18f62abd49ef36
JoseD-tech <jgmting@gmail.com>
2025-07-25 18:38:29 -0400

    practica de el curso de js

---
+++ app.js
@@ -0,0 +1,15 @@
+// Operadores Aritmeticos
+
+let numero1 = parseFloat(prompt("Ingresa el valor 1: "));
+let numero2 = parseFloat(prompt("Ingresa el valor 2: "));
+
+let suma = numero1 * numero2
+
+
+console.log(suma.toFixed(3));
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,41 @@
+// Tipos de datos : Primitivos
+
+// Numerico
+let edad = parseFloat(34); // Numero entero
+const numeroflotante = 10.1; // Numero con decimal .00
+
+// String
+let nombre = "Rogelio";
+
+// Bool = True y False
+
+// Indefinidos
+
+let dato // Undefined
+
+
+// Nulos
+
+let dato2 = null
+
+console.log(dato2);
+
+// Bigint
+
+let numeroGrande = 124124124124124141241241241241241241241212312312312312
+
+// Symbol: Valores unicos
+const variable = Symbol("variable")
+
+/**
+ * Operadores Logicos
+ * > : Mayor
+ * < : Menor
+ * >= : Mayor o igual
+ * <= : Menor o igual
+ * != : Diferente de
+ * == : Es igual
+ * === : Estrictamente igual
+ */
+
+
